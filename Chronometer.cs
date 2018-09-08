331d49444cb22bc9ac60a249973369dcd965c9e8
Nathan HANEN <hanen.nathan@gmail.com>
2018-09-08 18:43:02 +0200

    Added a Paused property to the Chronometer

--- Chronometer.cs
+++ Chronometer.cs
@@ -12,71 +12,79 @@ namespace WGP
     /// </summary>
     public class Chronometer
     {
-        private dynamic ReferenceTime { get; set; }
-        private Time Elapsed { get; set; }
+        private dynamic referenceTime;
+        private Time elapsed;
         private float speed;
-        private Time Buffer { get; set; }
-        private Time OldTime { get; set; }
-
+        private bool paused;
+        private Time buffer;
+        private Time oldTime;
         /// <summary>
-        /// Constructor.
+        /// If the chronometer is paused or not.
         /// </summary>
-        public Chronometer()
+        public bool Paused
         {
-            OldTime = Time.Zero;
-            ReferenceTime = new Clock();
-            Elapsed = SFML.System.Time.Zero;
-            Buffer = new Time();
-            speed = 1;
-        }
-        /// <summary>
-        /// Constructor. Used to set relative to another Chronometer.
-        /// </summary>
-        /// <param name="timer">Relative to.</param>
-        public Chronometer(Chronometer timer) : this()
-        {
-            OldTime = timer.ElapsedTime;
-            ReferenceTime = timer;
-            Elapsed = SFML.System.Time.Zero;
-            Buffer = new Time();
-            speed = 1;
+            get => paused;
+            set
+            {
+                Update();
+                paused = value;
+            }
         }
         /// <summary>
         /// Change the speed. The speed is the factor by which is multiplied the elapsed time.
         /// </summary>
         public float Speed
         {
-            get
-            {
-                return speed;
-            }
+            get => speed;
             set
             {
                 Update();
                 speed = value;
             }
         }
-        private void Update()
-        {
-            Buffer = ReferenceTime.ElapsedTime - OldTime;
-            OldTime = ReferenceTime.ElapsedTime;
-            Elapsed += Buffer * speed;
-        }
         /// <summary>
         /// The current elapsed time.
         /// </summary>
-        public Time ElapsedTime
+        public new Time ElapsedTime
         {
             get
             {
                 Update();
-                return Elapsed;
+                return elapsed;
             }
             set
             {
                 Update();
-                Elapsed = value;
+                elapsed = value;
             }
+        }
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Chronometer()
+        {
+            paused = false;
+            oldTime = Time.Zero;
+            referenceTime = new Clock();
+            elapsed = Time.Zero;
+            buffer = new Time();
+            speed = 1;
+        }
+        /// <summary>
+        /// Constructor. Used to set relative to another Chronometer. The main timer should not change its elapsed time to a lower value, as it may set a negative value to its childs.
+        /// </summary>
+        /// <param name="timer">Relative to.</param>
+        public Chronometer(Chronometer timer) : this()
+        {
+            oldTime = timer.ElapsedTime;
+            referenceTime = timer;
+        }
+        private void Update()
+        {
+            buffer = referenceTime.ElapsedTime - oldTime;
+            oldTime = referenceTime.ElapsedTime;
+            if (!Paused)
+                elapsed += buffer * Speed;
         }
         /// <summary>
         /// Restarts the chronometer.
@@ -84,7 +92,7 @@ namespace WGP
         public void Restart()
         {
             Update();
-            Elapsed = SFML.System.Time.Zero;
+            elapsed = Time.Zero;
         }
     }
 }
