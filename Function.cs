7cfed67e895faf67ba51db0f06e426e301ba48fa
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-04-28 20:16:42 +0200

    Added Function
    A customizable IFunction.

---
+++ Function.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WGP
+{
+    /// <summary>
+    /// Custom function.
+    /// </summary>
+    public class Function : IFunction
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public Function()
+        {
+            Fct = null;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="fct">Function used.</param>
+        public Function(Func<float, float> fct)
+        {
+            Fct = fct;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// Function called when trying to get the Image.
+        /// </summary>
+        public Func<float, float> Fct { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public float Image(float antecedent) => Fct == null ? throw new InvalidOperationException("Fct is not set") : Fct(antecedent);
+
+        #endregion Public Methods
+    }
+}
