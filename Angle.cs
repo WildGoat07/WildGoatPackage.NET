1791eba62868c83c193af6560995e20c9f420d72
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-04-28 20:13:47 +0200

    Angle changes
    - Radian is now a standalone property
    - The serialized version is "Radian" instead of "radians"

--- Angle.cs
+++ Angle.cs
@@ -13,40 +13,174 @@ namespace WGP
     [Serializable]
     public struct Angle : IComparable, IComparable<Angle>, IEquatable<Angle>, IFormattable, ISerializable, ICloneable
     {
+        #region Public Constructors
+
         /// <summary>
-        /// Angle corresponing to 0 degrees or 0 radians.
+        /// Constructor.
         /// </summary>
-        static public Angle Zero => FromRadians(0);
+        public Angle(Angle other) => Radian = other.Radian;
+
+        public Angle(SerializationInfo info, StreamingContext context)
+        {
+            Radian = info.GetSingle("Radians");
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
 
         /// <summary>
         /// Angle corresponing to 360 degrees or 2PI radians.
         /// </summary>
         static public Angle Loop => FromRadians(2 * (float)Math.PI);
 
-        private float radian;
+        /// <summary>
+        /// Angle corresponing to 0 degrees or 0 radians.
+        /// </summary>
+        static public Angle Zero => FromRadians(0);
 
         /// <summary>
         /// Angle in degrees.
         /// </summary>
         public float Degree
         {
-            get => radian * 180 / (float)Math.PI;
-            set => radian = value * (float)Math.PI / 180;
+            get => Radian * 180 / (float)Math.PI;
+            set => Radian = value * (float)Math.PI / 180;
         }
 
         /// <summary>
         /// Angle in radians.
         /// </summary>
-        public float Radian
-        {
-            get => radian;
-            set => radian = value;
-        }
+        public float Radian { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
 
         /// <summary>
-        /// Constructor.
+        /// Creates a new instance from a value in degrees.
         /// </summary>
-        public Angle(Angle other) => radian = other.radian;
+        /// <param name="degree">Value in degrees.</param>
+        /// <returns>New Angle instance.</returns>
+        static public Angle FromDegrees(float degree) => new Angle() { Degree = degree };
+
+        /// <summary>
+        /// Creates a new instance from a value in radians.
+        /// </summary>
+        /// <param name="radian">Value in radians.</param>
+        /// <returns>New Angle instance.</returns>
+        static public Angle FromRadians(float radian) => new Angle() { Radian = radian };
+
+        static public Angle operator -(Angle left, Angle right)
+        {
+            return FromRadians(left.Radian - right.Radian);
+        }
+
+        static public Angle operator -(Angle angle)
+        {
+            return FromRadians(-angle.Radian);
+        }
+
+        static public bool operator !=(Angle left, Angle right)
+        {
+            return !left.Equals(right);
+        }
+
+        static public Angle operator %(Angle left, Angle right)
+        {
+            return FromRadians(left.Radian % right.Radian);
+        }
+
+        static public Angle operator *(Angle angle, float value)
+        {
+            return FromRadians(angle.Radian * value);
+        }
+
+        static public Angle operator *(float value, Angle angle)
+        {
+            return FromRadians(angle.Radian * value);
+        }
+
+        static public Angle operator /(Angle angle, float value)
+        {
+            return FromRadians(angle.Radian / value);
+        }
+
+        static public Angle operator /(float value, Angle angle)
+        {
+            return FromRadians(value / angle.Radian);
+        }
+
+        static public float operator /(Angle left, Angle right)
+        {
+            return left.Radian / right.Radian;
+        }
+
+        static public Angle operator +(Angle left, Angle right)
+        {
+            return FromRadians(left.Radian + right.Radian);
+        }
+
+        static public bool operator <(Angle left, Angle right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        static public bool operator <=(Angle left, Angle right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        static public bool operator ==(Angle left, Angle right)
+        {
+            return left.Equals(right);
+        }
+
+        static public bool operator >(Angle left, Angle right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        static public bool operator >=(Angle left, Angle right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+
+        public object Clone() => new Angle(this);
+
+        public int CompareTo(object obj) => CompareTo((Angle)obj);
+
+        public int CompareTo(Angle other) => Radian.CompareTo(other.Radian);
+
+        /// <summary>
+        /// Returns the cosine of the angle.
+        /// </summary>
+        /// <returns>Cosine.</returns>
+        public float Cos() => (float)Math.Cos(Radian);
+
+        /// <summary>
+        /// Returns the hyperbolic cosine of the angle.
+        /// </summary>
+        /// <returns>Hyperbolic cosine.</returns>
+        public float Cosh() => (float)Math.Cosh(Radian);
+
+        public bool Equals(Angle other) => Radian.Equals(other.Radian);
+
+        public override bool Equals(object obj) => Radian.Equals(((Angle)obj).Radian);
+
+        /// <summary>
+        /// Generates a vector based on the angle.
+        /// </summary>
+        /// <param name="length">The length of the vector. (Optional)</param>
+        /// <returns>Generated vector.</returns>
+        public SFML.System.Vector2f GenerateVector(float length = 1) => RotateVector(new SFML.System.Vector2f(length, 0));
+
+        public override int GetHashCode() => Radian.GetHashCode();
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Radians", Radian);
+        }
 
         /// <summary>
         /// Returns the rotated vector corresponding to the angle.
@@ -67,71 +201,28 @@ namespace WGP
         public SFML.System.Vector2f RotateVector(float x, float y) => RotateVector(new SFML.System.Vector2f(x, y));
 
         /// <summary>
-        /// Generates a vector based on the angle.
-        /// </summary>
-        /// <param name="length">The length of the vector. (Optional)</param>
-        /// <returns>Generated vector.</returns>
-        public SFML.System.Vector2f GenerateVector(float length = 1) => RotateVector(new SFML.System.Vector2f(length, 0));
-
-        /// <summary>
-        /// Creates a new instance from a value in degrees.
-        /// </summary>
-        /// <param name="degree">Value in degrees.</param>
-        /// <returns>New Angle instance.</returns>
-        static public Angle FromDegrees(float degree) => new Angle() { Degree = degree };
-
-        /// <summary>
-        /// Creates a new instance from a value in radians.
-        /// </summary>
-        /// <param name="radian">Value in radians.</param>
-        /// <returns>New Angle instance.</returns>
-        static public Angle FromRadians(float radian) => new Angle() { Radian = radian };
-
-        /// <summary>
-        /// Returns the cosine of the angle.
-        /// </summary>
-        /// <returns>Cosine.</returns>
-        public float Cos() => (float)Math.Cos(radian);
-
-        /// <summary>
         /// Returns the sinus of the angle.
         /// </summary>
         /// <returns>Sinus.</returns>
-        public float Sin() => (float)Math.Sin(radian);
-
-        /// <summary>
-        /// Returns the tangent of the angle.
-        /// </summary>
-        /// <returns>Tangent.</returns>
-        public float Tan() => (float)Math.Tan(radian);
-
-        /// <summary>
-        /// Returns the hyperbolic cosine of the angle.
-        /// </summary>
-        /// <returns>Hyperbolic cosine.</returns>
-        public float Cosh() => (float)Math.Cosh(radian);
+        public float Sin() => (float)Math.Sin(Radian);
 
         /// <summary>
         /// Returns the hyperbolic sinus of the angle.
         /// </summary>
         /// <returns>Hyperbolic sinus.</returns>
-        public float Sinh() => (float)Math.Sinh(radian);
+        public float Sinh() => (float)Math.Sinh(Radian);
+
+        /// <summary>
+        /// Returns the tangent of the angle.
+        /// </summary>
+        /// <returns>Tangent.</returns>
+        public float Tan() => (float)Math.Tan(Radian);
 
         /// <summary>
         /// Returns the hyperbolic tangent of the angle.
         /// </summary>
         /// <returns>Hyperbolic tangent.</returns>
-        public float Tanh() => (float)Math.Tanh(radian);
-
-        public int CompareTo(object obj) => CompareTo((Angle)obj);
-
-        public int CompareTo(Angle other) => radian.CompareTo(other.radian);
-
-        public bool Equals(Angle other) => radian.Equals(other.radian);
-
-        public override bool Equals(object obj) => radian.Equals(((Angle)obj).radian);
-
-        public override int GetHashCode() => radian.GetHashCode();
+        public float Tanh() => (float)Math.Tanh(Radian);
 
         public override string ToString() => ToString(null, System.Globalization.CultureInfo.CurrentCulture);
 
@@ -153,91 +244,6 @@ namespace WGP
                 return "{ [Radians:" + Radian + "] , [Degrees:" + Degree + "] }";
         }
 
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("radians", Radian);
-        }
-
-        public object Clone() => new Angle(this);
-
-        public Angle(SerializationInfo info, StreamingContext context)
-        {
-            radian = info.GetSingle("radians");
-        }
-
-        static public Angle operator +(Angle left, Angle right)
-        {
-            return FromRadians(left.radian + right.radian);
-        }
-
-        static public Angle operator -(Angle left, Angle right)
-        {
-            return FromRadians(left.radian - right.radian);
-        }
-
-        static public Angle operator -(Angle angle)
-        {
-            return FromRadians(-angle.radian);
-        }
-
-        static public Angle operator *(Angle angle, float value)
-        {
-            return FromRadians(angle.radian * value);
-        }
-
-        static public Angle operator /(Angle angle, float value)
-        {
-            return FromRadians(angle.radian / value);
-        }
-
-        static public Angle operator *(float value, Angle angle)
-        {
-            return FromRadians(angle.radian * value);
-        }
-
-        static public Angle operator /(float value, Angle angle)
-        {
-            return FromRadians(value / angle.radian);
-        }
-
-        static public float operator /(Angle left, Angle right)
-        {
-            return left.radian / right.radian;
-        }
-
-        static public Angle operator %(Angle left, Angle right)
-        {
-            return FromRadians(left.radian % right.radian);
-        }
-
-        static public bool operator <(Angle left, Angle right)
-        {
-            return left.CompareTo(right) < 0;
-        }
-
-        static public bool operator >(Angle left, Angle right)
-        {
-            return left.CompareTo(right) > 0;
-        }
-
-        static public bool operator ==(Angle left, Angle right)
-        {
-            return left.Equals(right);
-        }
-
-        static public bool operator !=(Angle left, Angle right)
-        {
-            return !left.Equals(right);
-        }
-
-        static public bool operator <=(Angle left, Angle right)
-        {
-            return left.CompareTo(right) <= 0;
-        }
-
-        static public bool operator >=(Angle left, Angle right)
-        {
-            return left.CompareTo(right) >= 0;
-        }
+        #endregion Public Methods
     }
 }
